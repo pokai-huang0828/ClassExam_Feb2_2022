90426ddb60f74788a3eb1f1d7dedbd450197e5ee
Po-Kai Huang <71723684+pokai-huang0828@users.noreply.github.com>
2022-02-02 10:38:17 -0800

    Initial Commit

---
+++ ClassExam_Feb2_2022/ClassExam_Feb2_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  ClassExam_Feb2_2022App.swift
+//  ClassExam_Feb2_2022
+//
+//  Created by PoKai Huang on 2022-02-02.
+//
+
+import SwiftUI
+
+@main
+struct ClassExam_Feb2_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
