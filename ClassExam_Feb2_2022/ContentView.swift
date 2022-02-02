d6c97ae888ebe39fcedf0f6c3705af4aa80b19d6
Po-Kai Huang <71723684+pokai-huang0828@users.noreply.github.com>
2022-02-02 11:11:34 -0800

    Update File

--- ClassExam_Feb2_2022/ContentView.swift
+++ ClassExam_Feb2_2022/ContentView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name = ""
+    @State var nationality = ""
+    @State var showText = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: 0.20, green: 0.60, blue: 0.86)
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Text("Every Government Form Ever")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .padding(.bottom, 50.0)
+                VStack{
+                    HStack {
+                        Text("Name: ")
+                        TextField("", text: $name)
+                    }
+                    .padding(.all, 20)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                        
+                    HStack {
+                        Text("Nationality: ")
+                        TextField("", text: $nationality)
+                    }
+                    .padding(.all, 20)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    
+                    if showText == true {
+                        Text("Name: " + name)
+                        Text("Nationality: " + nationality)
+                    }
+                }
+                
+                
+                
+                
+                HStack{
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    
+                    Button {
+                        submitHandle()
+                    } label: {
+                        Text("Submit")
+                            .foregroundColor(Color.black)
+                            .padding(.all, 20)
+                            .background(Color.gray)
+                            .cornerRadius(20)
+                    }
+                    Spacer()
+                    Spacer()
+                    Circle()
+                        .fill(Color.gray)
+                        .frame(width: 80, height: 80)
+                    Spacer()
+                }.padding(/*@START_MENU_TOKEN@*/.all, 50.0/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+        }
+    }
+    func submitHandle(){
+        showText = true
     }
 }
 
